1572c783f1d901ab4839b4449fcfa3145f0e458e
Piotr Osiak <piotrosiak@digitaltreemedia.co.uk>
2018-01-31 23:19:40 +0000

    Update About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,28 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">A short introduction to this Weather App made in ReactJS</p>
+      <p className="text-center">
+        A very basic <strong className="label primary">Weather App</strong> built with <strong className="label warning">ReactJS</strong>.<br/> Project was worked on with the guidance of <span className="label alert">The Complete React Web App Developer Course</span>.
+      </p>
+      <ul className="menu">
+        <li>
+          <a href='https://github.com/PIIJEI/reactWeatherApp' className='text-center' target='_blank'>GitHub Project</a>
+        </li>
+        <li>
+          <strong>Author: Piotr &quot;PJ&quot;</strong>
+        </li>
+      </ul>
+      <h2>Tools Used:</h2>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript Framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used the Open Weather Map for API to gather the weather data by City Name.
+        </li>
+      </ul>
     </div>
   )
 };
