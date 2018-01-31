97a8e3748f7fa29ac37ce502a5e39d40ff12a350
Piotr Osiak <piotrosiak@digitaltreemedia.co.uk>
2018-01-31 23:32:09 +0000

    Styling added to the Main Weather App page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h4>There is {temp} degree in {location}</h4>
+    <h3 className="text-center">There is {temp} degree in {location}</h3>
   )
 };
 
