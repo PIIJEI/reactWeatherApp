cb4b3beb2252289422ebb451c62cfea1e388204c
Piotr Osiak <piotrosiak@digitaltreemedia.co.uk>
2018-02-01 19:25:13 +0000

    Added SASS

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,7 +8,7 @@ var Nav = React.createClass({
   },
   render: function() {
     return (
-      <div className="top-bar">
+      <div className="top-bar navbar-background">
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">React Weather App</li>
@@ -23,7 +23,7 @@ var Nav = React.createClass({
             </li>
           </ul>
         </div>
-        <div className="top-bar-right">
+        <div className="top-bar-right navbar-search">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
